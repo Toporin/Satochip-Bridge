e600ed49551a63903625b4e4889324903bf95cb8
Toporin <satochip.wallet@gmail.com>
2022-05-18 14:43:47 +0100

    Update Satochip-Bridge version to v0.4.0

--- satochip_bridge/version.py
+++ satochip_bridge/version.py
@@ -5,10 +5,11 @@
 # v0.2.2-0.11.2: support for pysatochip library 0.11.2
 # v0.2.3-0.11.2: windows & linux packaging
 # v0.2.4-0.11.2: patch - replace Exceptions with Error messages
-# v0.2.5-0.11.2: add prompts to confirm actions 
+# v0.2.5-0.11.2: add prompts to confirm actions
 # v0.2.6-0.11.2: change websocket port to 8397
 # v0.2.7-0.11.2: add support for offline 2FA reset
 # v0.3.0: SeedKeeper support & better error handling
 # v0.3.1: Add menu to select 2FA server in 2FA options
 # v0.3.2: Add menu to generate QR code from 2FA secret in 2FA options
-SATOCHIP_BRIDGE_VERSION = '0.3.2' # version of the client package
+# v0.4.0: Add WalletConnect support (beta)
+SATOCHIP_BRIDGE_VERSION = '0.4.0' # version of the client package
